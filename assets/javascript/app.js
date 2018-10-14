d047b26752d8cf3b9f97d4ab819351da6aaba82d
Dirk Kiesewetter <dirk.k1@outlook.com>
2018-10-14 16:44:34 -0400

    updated files & images

---
+++ assets/javascript/app.js
@@ -0,0 +1,18 @@
+/*
+Pseudocode
+
+start screen - onclick start button
+
+whn button clicked, go to questions page
+start timer
+decrement timer
+log each selection
+
+@ timer = 0, disallow any more input
+    display screen with win/loss totals
+
+    options - restart game
+
+    
+*/
+console.log("app.js active");
