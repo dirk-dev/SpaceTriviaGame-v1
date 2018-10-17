355c1f7f3311672eed958e6d2210ed8cfa62e4d0
Dirk Kiesewetter <dirk.k1@outlook.com>
2018-10-17 18:30:04 -0400

    updated js file

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,23 +1,68 @@
 /*
 Pseudocode
 
-start screen - onclick start button
+start screen - onclick start button*/
+var questions = [{
+        question: "What constellation is known as the Seven Sisters?",
+        answers: ["Big Dipper", "Pleiades", "Cassiopeia"],
+        correctAnswer: "Pleiades"
+    },
+    {
+        question: "What is the distance from the Earth to the Sun?",
+        answers: ["93 million miles", "1 light year", "500,000 miles"],
+        correctAnswer: "93 million miles"
+    },
+    {
+        question: "What is the largest known star?",
+        answers: ["Sirius", "Betelgeuse", "VY Canis Majoris"],
+        correctAnswer: "VY Canis Majoris"
+    } 
+    {
+        question: "What is the closest star to Earth?",
+        answers: ["Alpha Centauri", "Proxima Centauri", "Barnard's Star"],
+        correctAnswer: "Proxima Centauri"
+    },
+    {
+        question: "What is the closest star to Earth?",
+        answers: ["Alpha Centauri", "Proxima Centauri", "Barnard's Star"],
+        correctAnswer: "Proxima Centauri"
+    },
+    {
+        question: "How many moons does Jupiter have?",
+        answers: ["79", "8", "14"],
+        correctAnswer: "79"
+    },
+    {
+        question: "What is a shooting star?",
+        answers: ["comet", "meteor", "asteroid"],
+        correctAnswer: "meteor"
+    },
+    {
+        question: "How many planets in our solar system have rings?",
+        answers: ["one", "2", "4"],
+        correctAnswer: "4"
+    },
 
 
-when button clicked, go to questions page
+
+
+]
+/* when button clicked, go to questions page
 start timer
 decrement timer
 log each user selection
 
+if/else for validating answers
+
 @ timer = 0, disallow any more input
-hide question screen
+hide question screen - jQuery .show/.hide
 display screen with win/loss totals
 
 [possible options - restart game?
 
     USe JS for logic, JQuery to manipulate HTML
 
-    Don't forget to write a readme!!!
+    Don"t forget to write a readme!!!
 
     
 */
