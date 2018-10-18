14df2b6d6a306b33c624960efeb0c16a1ca00e19
Dirk Kiesewetter <dirk.k1@outlook.com>
2018-10-17 20:58:16 -0400

    updated js, html, and css

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,52 +1,67 @@
 /*
-Pseudocode
 
 start screen - onclick start button*/
-var questions = [{
-        question: "What constellation is known as the Seven Sisters?",
-        answers: ["Big Dipper", "Pleiades", "Cassiopeia"],
-        correctAnswer: "Pleiades"
-    },
-    {
-        question: "What is the distance from the Earth to the Sun?",
-        answers: ["93 million miles", "1 light year", "500,000 miles"],
-        correctAnswer: "93 million miles"
-    },
-    {
-        question: "What is the largest known star?",
-        answers: ["Sirius", "Betelgeuse", "VY Canis Majoris"],
-        correctAnswer: "VY Canis Majoris"
-    } 
-    {
-        question: "What is the closest star to Earth?",
-        answers: ["Alpha Centauri", "Proxima Centauri", "Barnard's Star"],
-        correctAnswer: "Proxima Centauri"
-    },
-    {
-        question: "What is the closest star to Earth?",
-        answers: ["Alpha Centauri", "Proxima Centauri", "Barnard's Star"],
-        correctAnswer: "Proxima Centauri"
-    },
-    {
-        question: "How many moons does Jupiter have?",
-        answers: ["79", "8", "14"],
-        correctAnswer: "79"
-    },
-    {
-        question: "What is a shooting star?",
-        answers: ["comet", "meteor", "asteroid"],
-        correctAnswer: "meteor"
-    },
-    {
-        question: "How many planets in our solar system have rings?",
-        answers: ["one", "2", "4"],
-        correctAnswer: "4"
-    },
+$(document).ready(function () {
+
+    var correctGuess = 0;
+    var wrongGuess = 0;
+
+    var questions = [{
+            question: "What constellation is also known as the Seven Sisters?",
+            answers: ["Big Dipper", "Pleiades", "Cassiopeia"],
+            correctAnswer: "Pleiades"
+        },
+        {
+            question: "What is the distance from the Earth to the Sun?",
+            answers: ["93 million miles", "1 light year", "500,000 miles"],
+            correctAnswer: "93 million miles"
+        },
+        {
+            question: "What is the largest known star?",
+            answers: ["Sirius", "Betelgeuse", "VY Canis Majoris"],
+            correctAnswer: "VY Canis Majoris"
+        },
+        {
+            question: "What is the closest star to Earth?",
+            answers: ["Alpha Centauri", "Proxima Centauri", "Barnard's Star"],
+            correctAnswer: "Proxima Centauri"
+        },
+        {
+            question: "What is the closest star to Earth?",
+            answers: ["Alpha Centauri", "Proxima Centauri", "Barnard's Star"],
+            correctAnswer: "Proxima Centauri"
+        },
+        {
+            question: "How many moons does Jupiter have?",
+            answers: ["79", "8", "14"],
+            correctAnswer: "79"
+        },
+        {
+            question: "What is a shooting star?",
+            answers: ["comet", "meteor", "asteroid"],
+            correctAnswer: "meteor"
+        },
+        {
+            question: "How many planets in our solar system have rings?",
+            answers: ["one", "2", "4"],
+            correctAnswer: "4"
+        },
+
+    ]
+    // removes start button onclick
+    $("button").click(function () {
+        $("#button1Div").remove();
+
+    });
+
+    //inserts question 1 into the HTML
+    $("#question1").text(questions[0].question);
+    $("#answer1A").text(questions[0].answers[0]);
 
 
 
 
-]
+});
 /* when button clicked, go to questions page
 start timer
 decrement timer
